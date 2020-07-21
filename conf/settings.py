420bebb3d4d2925756a34f863d3ec8be07cdac70
RamonaEJSpence <ramonaejspence@gmail.com>
2020-07-21 17:36:41 -0400

    correcting settings

--- conf/settings.py
+++ conf/settings.py
@@ -157,8 +157,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'frontend/static/build/static')
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/static/build/static'),)
