516faab277f24e1a7275826166c60956993330ac
RamonaEJSpence <ramonaejspence@gmail.com>
2020-07-21 14:44:24 -0400

    added indexview for frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import os
+import logging
 
-# Create your views here.
+from django.views.generic import View
+from django.http import HttpResponse
+from django.conf import settings
+
+class IndexView(View):
+    """
+    """
+
+    def get(self, request, *args, **kwargs):
+        try:
+            with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            logging.exception('Production build of this app not found')
+            return HttpResponse(
+                status = 501,
+            )
