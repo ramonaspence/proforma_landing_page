71d004b0bc548d2debb607bbdb88c0990a1371eb
RamonaEJSpence <ramonaejspence@gmail.com>
2020-07-24 17:21:26 -0400

    production build

--- data/urls.py
+++ data/urls.py
@@ -5,6 +5,13 @@ from . import views
 app_name = "data"
 
 
+handler401 = views.handler401
+hanlder403 = views.handler403
+handler404 = views.handler404
+handler500 = views.handler500
+
+
+
 urlpatterns = [
     path('contacts/', views.ContactListCreateAPIView.as_view(), name="contacts"),
 ]
--- data/views.py
+++ data/views.py
@@ -88,3 +88,14 @@ class ContactListCreateAPIView(generics.ListCreateAPIView):
 
         return super().create(request, *args, **kwargs)
 
+def handler403(request):
+    return render(request, '403.html', status=403)
+
+def handler401(request):
+    return render(request, '401.html', status=401)
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+def handler500(request):
+    return render(request, '500.html', status=500)
